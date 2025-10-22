bacd77dea0f2c277ba4e92219448a30e9c9bc6ba
Dawid Górnikowski <dawidgornikowski@o2.pl>
2025-10-23 00:14:48 +0200

    Separated backend and frontend

--- static/script.js
+++ static/script.js
@@ -1,1 +1,22 @@
-console.log("Kalkulator działa!");
+function appendNumber(num) {
+    const display = document.getElementById('display');
+    display.value += num;
+}
+
+function appendOperator(op) {
+    const display = document.getElementById('display');
+    display.value += op;
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Błąd';
+    }
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}

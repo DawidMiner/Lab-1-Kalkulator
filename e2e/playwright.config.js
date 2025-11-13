68c3e354984d9c67409769896f80bd2ac91db722
Dawid Górnikowski <dawidgornikowski@o2.pl>
2025-11-13 02:51:22 +0100

    little fix

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -2,6 +2,17 @@ module.exports = {
   use: {
     baseURL: process.env.BASE_URL || 'http://localhost:3000',
     headless: true,
-  }
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...this.use,
+        // przekazanie zmiennej do testów
+        env: {
+          BACKEND_URL: process.env.BACKEND_URL || 'http://localhost:5000',
+        },
+      },
+    },
+  ],
 };
-

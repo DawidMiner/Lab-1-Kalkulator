783468480ca5923c8a3da7954b44825d482b6099
Dawid Górnikowski <dawidgornikowski@o2.pl>
2025-11-13 02:39:28 +0100

    little fix

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,5 @@
-const API = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
+const API = (process.env.BACKEND_URL) ? process.env.BACKEND_URL : "http://localhost:5000";
+
 
 export function append(ch) {
   const d = document.getElementById("display");
